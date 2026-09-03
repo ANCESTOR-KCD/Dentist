9838c309ae4452d275008f8178a500149fdf7bea
ANCESTOR KCD <dubemchude@gmail.com>
2026-09-03 14:00:35 +0100

    superuser

---
+++ accounts/management/commands/ensure_admin.py
@@ -0,0 +1,23 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = "Ensure a superuser exists, reading credentials from environment variables."
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = os.environ.get('ADMIN_USERNAME')
+        password = os.environ.get('ADMIN_PASSWORD')
+        email = os.environ.get('ADMIN_EMAIL', '')
+
+        if not username or not password:
+            self.stdout.write('ADMIN_USERNAME or ADMIN_PASSWORD not set, skipping.')
+            return
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created.'))
+        else:
+            self.stdout.write(f'Superuser "{username}" already exists, skipping.')
